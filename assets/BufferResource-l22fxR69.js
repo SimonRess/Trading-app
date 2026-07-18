e20682f343f4e9408ed428d9291fbf71faf1802f
SimonRess <SimonRess@users.noreply.github.com>
2026-07-18 12:11:52 +0000

    deploy: 00ab03ed7302da205473deedfe2aed5bfa62161f

--- assets/BufferResource-l22fxR69.js
+++ assets/BufferResource-l22fxR69.js
@@ -1,4 +1,4 @@
-import{w as U,g,c,u as S,B as _,d as m,E as I,e as p}from"./index-BU0gAJKj.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,g,c,u as S,B as _,d as m,E as I,e as p}from"./index-Ds4J0WWo.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
