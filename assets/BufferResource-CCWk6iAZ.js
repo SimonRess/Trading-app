74b429a12a5989ea49eb8f4f17caec5f2e652957
SimonRess <SimonRess@users.noreply.github.com>
2026-08-02 07:12:48 +0000

    deploy: ce4981aa87fff7aaa92675b5138e4b95d199b140

--- assets/BufferResource-CCWk6iAZ.js
+++ assets/BufferResource-CCWk6iAZ.js
@@ -1,4 +1,4 @@
-import{w as U,g,c,u as S,B as _,d as m,E as I,e as p}from"./index-CBeF8hrP.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,g,c,u as S,B as _,d as m,E as I,e as p}from"./index-D10-D2u7.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
